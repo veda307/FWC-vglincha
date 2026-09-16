80e342badb894db28704870cfe6af01ac4b8599a
vglincha <vedag307@gmail.com>
2026-09-16 15:09:42 +0700

    cell03/ex00: background.html/css/js

---
+++ discovery_piscine/cell03/ex00/background.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("colorBtn");
+
+button.addEventListener("click", function () {
+	const r = Math.floor(Math.random() * 256);
+	const g = Math.floor(Math.random() * 256);
+	const b = Math.floor(Math.random() * 256);
+	document.body.style.backgroundColor = "rgb(" + r + ", " + g + ", " + b + ")";
+});
